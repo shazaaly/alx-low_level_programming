0d7831e73a16ddd67783d87dcc0f36bae53cf9dd
shazaaly <shaza.aly@gmail.com>
2023-03-16 07:12:22 -0700

    a

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This is entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%ld\n", (unsigned long)sizeof(int));
+	printf("%ld\n", (unsigned long)sizeof(float));
+	printf("%ld\n", (unsigned long)sizeof(char));
+	printf("%ld\n", (unsigned long)sizeof(double));
+        return (0);
+}
+
