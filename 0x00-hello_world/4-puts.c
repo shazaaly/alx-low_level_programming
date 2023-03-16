0c54b52d155dbbbd458882bda3b25b45960ece1e
shazaaly <shaza.aly@gmail.com>
2023-03-16 06:16:07 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 /*
 *Main function to print a string
 */
-int main(void)
+int main(void)    /* main fn display a text  */
 {
 puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
