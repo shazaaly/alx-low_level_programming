d9ed2c5c95843cbc92e5398a1e5c323b1a3c7d80
shazaaly <shaza.aly@gmail.com>
2023-03-17 11:18:33 -0700

    a

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,16 +10,15 @@ int main(void)
 	char lower[26];
 	char upper[26];
 
-	for (i= 0; i< 26 ; i++)
+	for (i = 0; i< 26 ; i++)
 	{
 	lower[i] = 'a' + i;
-        putchar(lower[i]);
+	putchar(lower[i])
 	}
-	putchar('\n');	
-	for (i= 0; i< 26 ; i++)
-        {
- 	 upper[i] = 'A' + i;
-         putchar(upper[i]);
+	for (i = 0; i< 26 ; i++)
+	{
+	upper[i] = 'A' + i;
+	putchar(upper[i]);
 	}
 	return (0);
 }
