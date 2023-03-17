872171a29db66be2cb6c3c453b4fdf4e41adcc84
shazaaly <shaza.aly@gmail.com>
2023-03-17 10:19:11 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/*
+ * main function entry
+ * Return : success : 0
+ */
+int main(void)
+{
+	char letters[26];
+	int i;
+	for(i = 0; i<26 ; i++)
+	{
+		letters[i] = 'a' + i;
+	}
+	
+	for(i = 0; i<26 ; i++)
+	{
+		putchar(letters[i]);
+	}
+	
+	
+	
+	
+	return (0);
+}
