b08c17a1693960533fe070e58d4651d81bc34327
shazaaly <shaza.aly@gmail.com>
2023-03-17 15:23:47 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - function : function prints letters
+ *
+ * This function prints all lowercase letters except for 'e' and 'q'.
+ *
+ * Return: 0 upon successful completion of the progra
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10 ; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
