7b8103720d45ac844eb22f8341b3d3aa9558cfd9
shazaaly <shaza.aly@gmail.com>
2023-03-17 10:24:30 -0700

    a

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,18 +8,16 @@ int main(void)
 {
 	char letters[26];
 	int i;
-	for(i = 0; i < 26 ; i++)
+
+	for (i = 0; i < 26 ; i++)
 	{
-		letters[i] = 'a' + i;
+	letters[i] = 'a' + i;
 	}
-	
-	for(i = 0; i < 26 ; i++)
+	for (i = 0; i < 26 ; i++)
 	{
-		putchar(letters[i]);
+	putchar(letters[i]);
 	}
-	
-	
-	
-	
+	putchar('\n');
+
 	return (0);
 }
