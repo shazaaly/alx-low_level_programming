94bf39d28f3e88314925be13240a704ad71db519
shazaaly <shaza.aly@gmail.com>
2023-03-17 11:26:13 -0700

    a

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
- *  * main function entry
+ * main function entry
  * Return : success : 0
  */
 int main(void)
@@ -10,12 +10,12 @@ int main(void)
 	char lower[26];
 	char upper[26];
 
-	for (i = 0; i< 26 ; i++)
+	for (i = 0; i < 26 ; i++)
 	{
 	lower[i] = 'a' + i;
 	putchar(lower[i]);
 	}
-	for (i = 0; i< 26 ; i++)
+	for (i = 0; i < 26 ; i++)
 	{
 	upper[i] = 'A' + i;
 	putchar(upper[i]);
