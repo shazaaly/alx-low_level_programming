72cb1ff2d784a3171d1656d10986f3083c760685
shazaaly <shaza.aly@gmail.com>
2023-03-19 02:44:01 -0700

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/*
+ * prints all possible combinations of single-digit numbers
+ *  use the putchar function
+ */
+
+/**
+ *  * main - function : function prints letters
+ *
+ * This function prints all lowercase letters except for 'e' and 'q'.
+ *
+ * Return: 0 upon successful completion of the progra
+ */
+int main(void)
+{
+	int i;
+
+	for (i=0; i < 10; i++)
+	{
+		if(i <= 8)
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar(i + '0');
+			putchar('$');
+		}
+	}
+	return (0);
+}
