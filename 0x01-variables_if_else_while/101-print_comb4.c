5af2b5ef65e86b71cdb09f974dfbd05fddbac041
shazaaly <shaza.aly@gmail.com>
2023-03-19 05:11:26 -0700

    a

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/*
+ * Write a program that prints all possible different
+ *  combinations of three digits.
+ */
+/**
+ *  * main - function : function prints letters
+ *
+ * This function prints all lowercase letters except for 'e' and 'q'.
+ *
+ * Return: 0 upon successful completion of the progra
+ */
+int main(void)
+{
+	int x,y,z;
+
+	for (x = 0; x <= 7; x++)
+	{
+		for (y = x + 1; y <= 8; y++)
+		{
+			for (z = y + 1; z <= 9; z++)
+			{
+				putchar(x + '0');
+				putchar(y + '0');
+				putchar(z + '0');
+				if (x != 7 || y != 8 || z != 9)
+				{
+					putchar(' ');
+					putchar(',');
+				}
+			}
+		}
+	}
+	putchar('\n');
+}
+
