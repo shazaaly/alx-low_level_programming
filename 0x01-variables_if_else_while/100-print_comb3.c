5730c4973b1d6185b47bdac32171fb753e4b80ee
shazaaly <shaza.aly@gmail.com>
2023-03-19 04:45:46 -0700

    a

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/*
+ * Write a program that prints all possible different
+ *  combinations of two digits.
+ */
+/**
+ *  * main - function : function prints letters
+ *
+ * This function prints all lowercase letters except for 'e' and 'q'.
+ *
+ * Return: 0 upon successful completion of the progra
+ */
+int main(void)
+{
+	int i, j;
+
+	for ( i = 0; i <= 8; i++)
+	{
+		for(j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if(i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+
+
+}
