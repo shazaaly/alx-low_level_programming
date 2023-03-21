7e493a86bf65eb9458f8f9b38ab99e4acba89906
shazaaly <shaza.aly@gmail.com>
2023-03-21 05:57:16 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ * function to print alphabet ten times
+ * /
+/**
+*  * main - function : function prints letters
+*
+* This function prints all lowercase letters except for 'e' and 'q'.
+*
+* Return: 0 upon successful completion of the progra
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
+
