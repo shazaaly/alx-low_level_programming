12af7210c0ff39b60b4870ff39a78e6d2fcfc917
shazaaly <shaza.aly@gmail.com>
2023-03-21 06:35:28 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,7 @@
 *in lowercase, followed by a new line.
 */
 /**
-*  *print_alphabet_x10 : function prints lettersi ten times.
+*  *print_alphabet_x10 -  function prints lettersi ten times.
 *
 * This function prints all lowercase letters except for 'e' and 'q'.
 *
