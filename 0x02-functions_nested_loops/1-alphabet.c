da45fb3ac0ca1feb28e51b850809919139e64000
shazaaly <shaza.aly@gmail.com>
2023-03-21 05:15:50 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <main.h>
+/*
+ * Write a function that prints the alphabet, 
+ * in lowercase, followed by a new line.
+ */
+/**
+ *  * main - function : function prints letters
+ *
+ * This function prints all lowercase letters except for 'e' and 'q'.
+ *
+ * Return: 0 upon successful completion of the progra
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++))
+	{
+		_putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
