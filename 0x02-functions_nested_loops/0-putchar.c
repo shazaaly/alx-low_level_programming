95adf5581473f4c73eee48defd3c56f446038552
shazaaly <shaza.aly@gmail.com>
2023-03-21 04:49:34 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -15,10 +15,9 @@ int main(void)
 	char *str = "_putchar";
 	int i;
 
-    for (i = 0; i < 8; i++)
+    for(i = 0; i < 8; i++)
     {
        _putchar(str[i]);
     }
-    return (0);
     putchar('\n');
-}
+    return (0);
