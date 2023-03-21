6bb3a5250e5894bc13918f329fffe190c1edc25c
shazaaly <shaza.aly@gmail.com>
2023-03-21 03:36:22 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+ * Write a program that prints _putchar, followed by a new line.
+ */
+/**
+ *  * main - function : function prints letters
+ *
+ * This function prints all lowercase letters except for 'e' and 'q'.
+ *
+ * Return: 0 upon successful completion of the progra
+ */
+int main(void)
+{
+	char *str = "_putchar";
+	int i;
+
+    for (i = 0; i < 8; i++)
+    {
+        putchar(str[i]);
+    }
+    return (0);
+}
