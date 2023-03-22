5d71d090441ac11e733ce89eeac82515076d9d7c
shazaaly <shaza.aly@gmail.com>
2023-03-22 01:08:40 -0700

    a

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/*
+* print_sign - function that checks for lowercase character.
+* return 1 or 0
+*/
+/**
+* * print_sign - function prints letters small caps.
+*
+*This function prints all sign.
+*Parameters - intege
+* Returns 0 and prints 0 if n is zero
+* Returns -1 and prints - if n is less than zero
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if ( n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+	else
+	{
+		putchar('0');
+		return (0);
+	}
+	return (0);
+}
