faeeab3f7bb1aa5bb331c307f552032d5cde4ecf
shazaaly <shaza.aly@gmail.com>
2023-03-22 01:32:36 -0700

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,16 +17,19 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar('+');
+		putchar('1');
 		return (1);
 	}
 	else if ( n == 0)
 	{
+		putchar('0');
 		putchar('0');
 		return (0);
 	}
 	else
 	{
 		putchar('-');
+		putchar('1');
 		return (-1);
 	}
 }
