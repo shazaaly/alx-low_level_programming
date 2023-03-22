6d191889b6640f9255815c7b66768983f6901589
shazaaly <shaza.aly@gmail.com>
2023-03-22 01:29:50 -0700

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,17 +17,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar('+');
-		putchar('1');
+		return (1);
 	}
-	else if ( n < 0)
+	else if ( n == 0)
 	{
-		putchar('-');
-		putchar('1');
+		putchar('0');
+		return (0);
 	}
 	else
 	{
-		putchar('0');
-		putchar('0');
+		putchar('-');
+		return (-1);
 	}
-	return (0);
 }
