95ef541ea3015f918f26feed8fb0d0d87dfc75a7
shazaaly <shaza.aly@gmail.com>
2023-03-22 01:43:41 -0700

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,8 +7,8 @@
 /**
 * * print_sign - function prints letters small caps.
 *
-*This function prints all sign.
-*Parameters - intege
+* This function prints all sign.
+* Parameters - intege
 * Returns 0 and prints 0 if n is zero
 * Returns -1 and prints - if n is less than zero
 */
@@ -17,19 +17,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar('+');
-		putchar('1');
 		return (1);
 	}
 	else if ( n == 0)
 	{
-		putchar('0');
 		putchar('0');
 		return (0);
 	}
 	else
 	{
 		putchar('-');
-		putchar('1');
 		return (-1);
 	}
 }
