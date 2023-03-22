6c7ed1381ebcd9e3f7d1b3fd8437d7ed526eea40
shazaaly <shaza.aly@gmail.com>
2023-03-22 03:38:39 -0700

    a

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int x
+	int y;
+
+	for (i = 0; i < 3; i++)
+	{
+        	for (j = 0; j < 4; j++)
+        	{
+            		for (x = 0; x < 6; x++)
+            		{
+                		for (y = 0; y < 9; y++)
+                		{
+                    			putchar(i + '0');
+                    			putchar(j + '0');
+                    			putchar(58);
+                    			putchar(x + '0');
+                    			putchar(y + '0');
+                    			putchar('\n');
+                }
+            }
+        }
+    }
+
+    return 0;
+}
