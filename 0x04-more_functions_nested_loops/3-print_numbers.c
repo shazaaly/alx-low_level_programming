8c29bd0babe64dc7c3692faa1b030e269862050e
shazaaly <shaza.aly@gmail.com>
2023-03-24 15:31:50 -0700

    a

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,9 +9,9 @@ void print_numbers(void)
 {
 	int num = 0;
 
-	do{
+	do {
 		_putchar(num + 48);
-		num ++;
-	}while (num >= 0 && num <= 9);
+		num++;
+	} while (num >= 0 && num <= 9);
 	_putchar('\n');
 }
