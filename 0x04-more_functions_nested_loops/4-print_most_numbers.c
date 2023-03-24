7e8f431c323daaad9a0ad45656e1a98a34724762
shazaaly <shaza.aly@gmail.com>
2023-03-24 16:35:50 -0700

    a

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,15 +8,15 @@
 */
 void print_most_numbers(void)
 {
-	int num = 0;
+	int num;
 
-	do {
+	for (num = 0; num <= 9; num++)
+	{
 		if (num == 2 || num == 4)
 		{
-			num++;
 			continue;
 		}
 		_putchar(num + 48);
-	} while (num >= 0 && num <= 9)
+	}
 	_putchar('\n');
 }
