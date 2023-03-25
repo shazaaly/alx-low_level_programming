a9d17113f220005b20250b68d5b5b1a1d53c76fb
shazaaly <shaza.aly@gmail.com>
2023-03-25 16:15:04 -0700

    a

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -20,12 +20,12 @@ for (i = 1; i <= 100; i++)
 	printf("FizzBuzz");
 	printf(" ");
 	}
-	else if (i % 3 == 0 && !(i % 5 == 0))
+	else if (i % 3 == 0)
 	{
 		printf("Fizz");
 		printf(" ");
 	}
-	else if (i % 5 == 0 && !(i % 3 == 0))
+	else if (i % 5 == 0)
 	{
 		printf("Buzz");
 		printf(" ");
