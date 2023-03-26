f53762bd799daa56a1d5f4068e7b17905ebe846f
shazaaly <shaza.aly@gmail.com>
2023-03-25 18:06:21 -0700

    a

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_integer - Prints an integer to the standard output
+*
+* @n: The integer to print
+*
+* This function takes an integer @n as input and prints it to the standard
+* Return: None
+*/
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if((n / 10) > 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 1)0 + 48);
+}
