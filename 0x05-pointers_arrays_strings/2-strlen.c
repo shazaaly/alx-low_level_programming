23b4aab701b4f5c9b765648bb71a3503861f5932
shazaaly <shaza.aly@gmail.com>
2023-03-27 16:07:28 -0700

    a

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,9 +4,9 @@
 * _strlen - returns the length of a string
 * @s: a pointer to the string
 *
-* This function takes a pointer to a 
+* This function takes a pointer to a
 * string and counts the number of characters
-* in the string using a loop. It returns the length of 
+* in the string using a loop. It returns the length of
 * the string as an integer.
 *
 * Return: the length of the string as an integer.
