def202ec8d583e0a7be4305999515f0f2027017a
shazaaly <shaza.aly@gmail.com>
2023-03-27 14:36:41 -0700

    a

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* @brief Resets the value of an integer variable to 98.
+* update_to_98 - updates the value of an integer variable to 98
+* @n: a pointer to an integer variable
 *
 * This function takes a pointer to an integer variable and updates its value
 * to 98 using the dereference operator (*).
-*
-* @param n A pointer to an integer variable.
 */
 void reset_to_98(int *n)
 {
