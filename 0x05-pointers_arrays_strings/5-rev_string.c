8eae3652ff484e2b79b68f005bdb75477fe33968
shazaaly <shaza.aly@gmail.com>
2023-03-27 18:28:18 -0700

    a

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* reverse_string - Reverses a string
+*
+* @str: The string to reverse
+*/
+void rev_string(char *s)
+{
+	char rev;
+	int i = 0;
+
+    while (s[i])
+    {
+        i++;
+    }
+
+    while (i--)
+    {
+        rev = s[i];
+    }
+}
