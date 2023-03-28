485f6e23a21c341dd4846d6d86f44eaadab5476d
shazaaly <shaza.aly@gmail.com>
2023-03-28 05:17:25 -0700

    a

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,27 +6,23 @@
 */
 void puts_half(char *str)
 {
+	int i = 0;
+	int j;
 
-	 int i = 0;
-    int j;
-
-    while (str[i] != '\0')
-    {
-        ++i;
-    }
-
-    if (i % 2 == 0)
-    {
-        j = i / 2;
-        for (; j < i; j++)
-        {
-            _putchar(str[j]);
-        }
-    }
-    else
-    {
-        j = i / 2 + 1;
-        _putchar(str[j]);
-    }
-    _putchar('\n');
+	while (str[i] != '\0')
+	{
+		++i;
+	}
+	if (i % 2 == 0)
+	{
+		j = i / 2;
+		for (; j < i; j++);
+		_putchar(str[j]);
+	}
+	else
+	{
+		j = i / 2 + 1;
+		_putchar(str[j]);
+	}
+	_putchar('\n');
 }
