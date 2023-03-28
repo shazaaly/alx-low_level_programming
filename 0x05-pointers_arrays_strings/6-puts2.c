5bea4584de8d52a69b728ab2b2a911ddbb7fb046
shazaaly <shaza.aly@gmail.com>
2023-03-28 02:54:57 -0700

    a

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,8 +12,10 @@ void puts2(char *str)
 	{
 		for (i = 0; str[i] != '\0'; i += 2)
 		{
-			_putchar(str[i] + 0);
-		}
+			if (i % 2 == 0)
+			{
+			_putchar(str[i]);
+			}
 	str--;
 	}
 	_putchar('\n');
