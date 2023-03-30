38ad3e29b9ebef1cc6a205538c8a0a5f25cebaf3
shazaaly <shaza.aly@gmail.com>
2023-03-30 08:09:00 -0700

    a

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -20,3 +20,17 @@ void print_number(int n)
 	}
 	_putchar(n % 10 + '0');
 }
+int main(void)
+{
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
