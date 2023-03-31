2a9a1b3abf9e40b99ecbe1acea875b65b05bf781
shazaaly <shaza.aly@gmail.com>
2023-03-30 17:46:19 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,11 @@ char *cap_string(char *str)
 	
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		 if ((str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+             str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+             str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+             str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+             str[i - 1] == '}') && (str[i] >= 'a' && str[i] <= 'z'))
 		{
 			str[i] = str[i] - 32;
 		}
