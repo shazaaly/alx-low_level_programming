6fa57360b117668d0251f856cc47669dabb913c2
shazaaly <shaza.aly@gmail.com>
2023-03-30 17:27:21 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
 * cap_string - Capitalizes all words in a string.
 * @str: Pointer to the string to be modified.
@@ -8,7 +7,7 @@
 char *cap_string(char *str)
 {
 	int i;
-	char *ptr =str;
+	char *ptr = str;
 
 	while (str[i] != '\0')
 	{
@@ -19,14 +18,4 @@ char *cap_string(char *str)
 		i++;
 	}
 	return (ptr);
-}
-int main(void)
-{
-    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-    char *ptr;
-
-    ptr = cap_string(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
 }
