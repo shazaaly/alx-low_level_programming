a7716c8f9bb305a280a023e01aab6fbb767ae466
shazaaly <shaza.aly@gmail.com>
2023-03-30 18:26:17 -0700

    a

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,10 +15,6 @@ int _strcmp(char *s1, char *s2)
 		    {
 			    return ((int)*s1 - (int)*s2);
 		    }
-		    else
-		    {
-			    return ((int)*s1 - (int)*s2);
-		    }
 		    s1++;
 		    s2++;
 	    }
