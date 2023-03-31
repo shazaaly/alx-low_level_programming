d09c9ccd327df24a5607c0ca0d8f01862a2d567b
shazaaly <shaza.aly@gmail.com>
2023-03-30 17:12:10 -0700

    a

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,7 +11,36 @@
 */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int *ptr = **r;
-	r = n1 + n2;
-	return (ptr);
+	int i = 0, j = 0, large = 0;
+
+	while (n1[i] != '\0')
+	{
+		i++;
+	}
+	while (n2[j] != '\0')
+	{
+		j++;
+	}
+	if (i > j)
+	{
+		large = i;
+	}
+	else
+	{
+		large = j;
+	}
+	/* check if larger sting of ints is > size*/
+	if (large + 1 > size_r)
+	{
+		return (0);
+	}
+	r[large] = '\0';
+	while (i >= 0 || j >= 0)
+	{
+		int digit1  = n1[i] - '0';
+		int digit2 = n2[j] - '0';
+		int sum = digit1 + digit2;
+
+	}
+	return r;
 }
