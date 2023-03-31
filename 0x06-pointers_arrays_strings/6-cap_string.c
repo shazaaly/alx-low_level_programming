8a6e794e5bc079998d4a4e1679e87c96c288e303
shazaaly <shaza.aly@gmail.com>
2023-03-30 17:53:54 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,18 +10,14 @@ char *cap_string(char *str)
 	
 	while (str[i] != '\0')
 	{
-		 if ((str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
-             str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
-             str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
-             str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
-             str[i - 1] == '}') && (str[i] >= 'a' && str[i] <= 'z'))
+		if ((str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+		str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+		str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+		str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+		str[i - 1] == '}') && (str[i] >= 'a' && str[i] <= 'z'))
 		{
 			str[i] = str[i] - 32;
 		}
-		 else if (str[i] >= 'A' && str[i] <= 'Z')
-		 {
-			 str[i] = str[i] + 32;
-		 }
 		i++;
 	}
 	return (str);
