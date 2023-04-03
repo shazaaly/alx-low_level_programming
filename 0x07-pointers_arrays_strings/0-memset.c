240075eeec56c84504621f2c0ca0d839e4364672
shazaaly <shaza.aly@gmail.com>
2023-04-03 23:39:22 +0200

    a

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,10 +11,20 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
+	if (n == 0)
+	{
+		return (s);
+	}
 	for (i = 0; i <= n; i++)
 	{
-		s[i] = b;
+			if (b == 0)
+			{
+				s[i] = 0;
+			}
+			else
+			{
+				s[i] = b;
+			}
 	}
 	return (s);
-
-}
+	}
