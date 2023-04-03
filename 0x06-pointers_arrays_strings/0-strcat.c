b771f509acaaa20363fdf55aa5e8bb7e4675f7b4
shazaAly <shaza.aly@gmail.com>
2023-04-03 17:53:00 +0200

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,15 +2,12 @@
 #include <string.h>
 /**
 * _strcat - Concatenates two strings.
-*
 * @dest: Pointer to the destination string.
 * @src: Pointer to the source string.
-*
 * Return: Pointer to the resulting string @dest.
 */
 char *_strcat(char *dest, char *src)
 {
 	strcat(dest, src);
 	return (dest);
-
 }
