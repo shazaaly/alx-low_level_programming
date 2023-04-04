01921bd911f66e9b210e6c7bbbf784f41447e064
shazaaly <shaza.aly@gmail.com>
2023-04-04 02:33:22 +0200

    a

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,9 +12,12 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i;
 	unsigned int j;
 
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (s[i] == '\0')
+		{
+			break;
+		}
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
