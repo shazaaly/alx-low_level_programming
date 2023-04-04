af5636d9678ea68f3bc70a57b7bf0538f655ba40
shazaaly <shaza.aly@gmail.com>
2023-04-04 14:33:28 +0200

    a

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,13 +10,13 @@ char *_strstr(char *haystack, char *needle)
 {
 	unsigned int i, j;
 
-	for (i = 0; needle[i] != '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; haystack[j] != '\0'; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (needle[i] == haystack[j])
+			if (haystack[i] == needle[j])
 			{
-				return (needle);
+				return (haystack + i);
 			}
 		}
 	}
