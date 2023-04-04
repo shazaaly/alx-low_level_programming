0a531c05ea1e7f669315118c018582e7160d8a26
shazaaly <shaza.aly@gmail.com>
2023-04-04 14:30:37 +0200

    a

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,5 +8,17 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-	return (needle);
+	unsigned int i, j;
+
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+		for (j = 0; haystack[j] != '\0'; j++)
+		{
+			if (needle[i] == haystack[j])
+			{
+				return (needle);
+			}
+		}
+	}
+	return (NULL);
 }
