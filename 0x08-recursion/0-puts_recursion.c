2cfa201d3588b958e52b3ab8a5458f57659a0cc8
shazaaly <shaza.aly@gmail.com>
2023-04-06 00:12:15 +0200

    a

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 #include "main.h"
-#include <unistd.h>
 /**
 * _puts_recursion - prints a string followed by a new line
 * @s: pointer to string to print
-*
 * Return: void
 */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	while (*s != '\0')
 	{
-		return;
-	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
 	_putchar('\n');
+	}
 }
--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -1,12 +1,28 @@
 #include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+/**
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+ int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
 int main(void)
 {
-    _puts_recursion("Puts with recursion");
-    return (0);
+	char *s = "Shaza Alu";
+		if (*s == '\0')
+	{
+		return (0);
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	_putchar('\n');
+     return (0);
 }
