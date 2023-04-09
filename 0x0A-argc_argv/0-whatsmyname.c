fad4d34a6d59dd50d9c9974ee9f055693b73a14f
shazaaly <shaza.aly@gmail.com>
2023-04-09 23:56:30 +0200

    a

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
 * main - Entry point
 * @argc: The number of command-line arguments
 * @argv: An array of command-line argument strings
-* Return: Always 0
+* Return: 0
 */
 int main(int argc, char *argv[])
 {
