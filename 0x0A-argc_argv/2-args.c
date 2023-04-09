d390ea34cc9d8fc2621ad02d6184a641b4b7246e
shazaaly <shaza.aly@gmail.com>
2023-04-10 01:11:30 +0200

    a

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* @argc: The number of command-line arguments
+* @argv: An array of command-line argument strings
+* Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
