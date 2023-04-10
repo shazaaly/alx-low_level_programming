ba2beebbf9c9d356e42dd3f8835cadfaf9ee1399
shazaaly <shaza.aly@gmail.com>
2023-04-10 03:07:37 +0200

    a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,20 +14,20 @@ int main(int argc, char *argv[])
 	int coins = 0;
 	int cents[] = {25, 10, 5, 2, 1};  /*sorted descend*/
 	int size = sizeof(cents) / sizeof(int);
+	int amount = atoi(argv[1]);
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int amount = atoi(argv[1]);
 
 	if (amount < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-/*	for (i = 0; argv[1][i] != '\0'; i++)
+	for (i = 0; argv[1][i] != '\0'; i++)
 	{
 		if (!isdigit(argv[1][i]))
 		{
@@ -35,7 +35,6 @@ int main(int argc, char *argv[])
 			return (1);
 		}
 	}
-	*/
 
 		for (i = 0; i < size; i++)
 		{
