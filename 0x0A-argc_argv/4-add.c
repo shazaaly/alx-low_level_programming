73ec61163c470bdad07d1e58025cc73bec3ac403
shazaaly <shaza.aly@gmail.com>
2023-04-10 02:10:31 +0200

    a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,11 +13,10 @@ int main(int argc, char *argv[])
 	int sum = 0;
 
 	if (argc == 1)
-		{
-			printf("0\n");
-			return (0);
-		}
-
+	{
+		printf("0\n");
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
