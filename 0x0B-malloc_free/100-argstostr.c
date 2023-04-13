a3b5af83293d56071a1c96e2b7846ffbc319e488
shazaaly <shaza.aly@gmail.com>
2023-04-13 05:01:42 +0200

    a

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -5,8 +5,24 @@
 
 char *argstostr(int ac, char **av)
 {
+	int i;
+
 	if (ac == 0 || av == NULL)
 	{
-        return NULL;
+        return (NULL);
     }
+	int tlength = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		char *arg = av[i];
+		int len = 0;
+
+		while (arg[len] != '\0')
+		{
+			len++;
+		}
+		tlength = len + 1;
+	}
+	   char *str = (char *) malloc(total_len * sizeof(char));
 }
