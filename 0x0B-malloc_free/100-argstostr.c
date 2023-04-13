295bed964b5ac995735ea4375e0310117d9d3f0e
shazaaly <shaza.aly@gmail.com>
2023-04-13 04:51:49 +0200

    a

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,28 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char **strtow(char *str)
+char *argstostr(int ac, char **av)
 {
-	int i = 0;
-	int length = 0;
-
-	while (str[i] != '\0')
+	if (ac == 0 || av == NULL)
 	{
-		length++;
-		i++;
-	}
-
-	char *p = malloc((length + 1) * sizeof(char));
-
-	if (p == NULL)
-	{
-		return (NULL);
-	}
-	if (str == NULL || *str == '\0')
-	{
-        return (NULL);
+        return NULL;
     }
-
-
-
 }
