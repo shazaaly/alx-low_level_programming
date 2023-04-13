68a6a1fae6076942692a84c21712dfe3f2bd5a07
shazaaly <shaza.aly@gmail.com>
2023-04-13 02:56:40 +0200

    a

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -31,7 +31,7 @@ int **alloc_grid(int width, int height)
 		{
 			return (NULL);
 		}
-		for (j = 0; j < width; j++)
+		for (j = 0; j < width - 1; j++)
 		{
 			ptr[i][j] = 0;
 		}
--- 0x0B-malloc_free/3-main.c
+++ 0x0B-malloc_free/3-main.c
@@ -1,9 +1,16 @@
-
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-void print_grid(int **grid, int width, int height)
 
+/**
+ * print_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+void print_grid(int **grid, int width, int height)
 {
     int w;
     int h;
