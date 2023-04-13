6438631bde0ce2ff11943302e62f499fa22e9841
shazaaly <shaza.aly@gmail.com>
2023-04-13 04:40:11 +0200

    a

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,20 +8,22 @@ char **strtow(char *str)
 	int i = 0;
 	int length = 0;
 
-	if (str == NULL || *str == '\0')
-	{
-        return (NULL);
-    }
 	while (str[i] != '\0')
 	{
 		length++;
 		str++;
 	}
+
 	char *p = malloc((length + 1) * sizeof(char));
+
 	if (p == NULL)
 	{
 		return (NULL);
 	}
+	if (str == NULL || *str == '\0')
+	{
+        return (NULL);
+    }
 
 
 
