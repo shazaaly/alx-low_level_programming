20f4ad40bba356ff228fc26a9b8aed397e039a5c
shazaaly <shaza.aly@gmail.com>
2023-04-15 00:51:25 +0200

    a

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,6 +16,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int j;
 	char *ptr;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s1 == NULL)
+	{
+		s2 = "";
+	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		len1++;
