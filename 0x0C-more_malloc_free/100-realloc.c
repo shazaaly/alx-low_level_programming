4983c7bcbb557d56ddd150a1deaeebef19973510
shazaaly <shaza.aly@gmail.com>
2023-04-15 04:45:54 +0200

    a

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -34,8 +34,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (ptr == NULL)
 	{
-		new = malloc(new_size);
-		free(ptr);
+		ptr = malloc(new_size);
 	}
 	if (new_size > old_size)
 	{
