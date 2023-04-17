73af9fbda0ba66efe2b81b74f02b1e464aa43b9f
shazaaly <shaza.aly@gmail.com>
2023-04-18 01:41:57 +0200

    a

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+/**
+* init_dog - initializes a dog structure with given values
+* @d: pointer to a struct dog to be initialized
+* @name: pointer to a char representing the dog's name
+* @age: float representing the dog's age
+* @owner: pointer to a char representing the dog's owner's name
+* Description: This function takes in a pointer to a struct dog and initializes
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
