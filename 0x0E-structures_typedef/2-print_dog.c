1ceadc845668217acbc38ff862bba49e25346f09
shazaaly <shaza.aly@gmail.com>
2023-04-18 04:39:20 +0200

    a

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -23,6 +23,6 @@ void print_dog(struct dog *d)
 		d->owner = "nil";
 	}
 	printf("Name: %s\n", d->name);
-	printf("Age: %.1f\n", d->age);
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
 }
