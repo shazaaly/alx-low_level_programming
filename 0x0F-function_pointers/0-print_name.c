cf5dd445d77af1a728a7e561a00cf88dac7d2e94
shazaaly <shaza.aly@gmail.com>
2023-04-22 11:13:33 +0200

    a

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,7 @@
+#include "function_pointers.h"
+
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,5 @@
+#ifndef FUNC_POINTERS_H
+#define FUNC_POINTERS_H
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#endif
