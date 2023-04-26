f6cba6cd46a1a815b193092cf84fbe52faa0b49e
shazaaly <shaza.aly@gmail.com>
2023-04-26 13:00:15 +0200

    fix while counter

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,6 +10,7 @@
 
 int (*get_op_func(char *s))(int, int)
 {
+	int i = 0;
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -18,15 +19,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 };
-int i = 0;
-
 while (ops[i].op != NULL)
 {
 if (*(ops[i].op) == *s)
 {
 	return (ops[i].f);
-	i++;
 }
+	i++;
 }
 return (NULL);
 }
