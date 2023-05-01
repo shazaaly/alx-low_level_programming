837316f51e22c341f797e1a8fe7bec50e101adc5
root <root@shaza>
2023-05-01 09:40:34 +0200

    betty add

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,7 +2,12 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
-
+/**
+* add_node_end - Adds a new node to the end of a singly linked list
+* @head: Pointer to the head of the list
+* @str: String to add as the data in the new node
+* Return: Pointer to the new node, or NULL if memory allocation fails
+*/
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *ptr = *head;
