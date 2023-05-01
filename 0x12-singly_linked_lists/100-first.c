3c46a2b3263f337a9438d25782268286dd28cac2
root <root@shaza>
2023-05-01 16:09:15 +0200

    betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,16 @@
 #include "lists.h"
 #include <stdio.h>
 /**
-* printList - Prints the strings in a linked list
-* @head: Pointer to the head of the list
+* myStartupFun - Function to execute before main()
+* This function is a constructor that will be automatically called before main
+* is executed. It prints the message "You're beat! and yet, you must allow,"
+* followed by "I bore my house upon my back!" to the console.
 */
-void myStartupFun (void) __attribute__ ((constructor));
-void myStartupFun (void)
+void myStartupFun(void) __attribute__ ((constructor));
+void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
+
 
