98b15253415c6a4da121f73f9ae9a479225b1e99
root <root@shaza>
2023-05-01 15:29:22 +0200

    print str betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,14 +3,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
+/**
+* printList - Prints the strings in a linked list
+* @head: Pointer to the head of the list
+*/
 void printList(list_t *head)
 {
 	list_t *node = head;
+
 	while (node != NULL)
 	{
-		printf("%s",node->str);
+		printf("%s", node->str);
 		node = node->next;
 	}
 
 }
+
