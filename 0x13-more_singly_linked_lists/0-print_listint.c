0cc311eb22ee3bf5c195bf3b97c684431d80134a
root <root@shaza>
2023-05-01 19:53:10 +0200

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include "stdio.h"
+#include <stddef.h>
+/**
+* print_listint - Prints the values in a linked list of integers
+*
+* @h: Pointer to the head of the list
+* Return: The number of nodes in the list
+*/
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *ptr = h;
+	size_t count = 0;
+
+	while (ptr != NULL)
+	{
+		printf("%d", ptr->n);
+		ptr = ptr->next;
+		count++;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include "list_struct.c"
+
+char _putchar(char c);
+size_t print_listint(const listint_t *h);
+#endif
+
