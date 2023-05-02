e71418c5607b64d6e0c0991b10bdc64fe2b179fa
root <root@shaza>
2023-05-02 22:16:58 +0200

    free list betty

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,13 +3,18 @@
 #include <stddef.h>
 #include <stdlib.h>
 /**
-* free_listint - Frees a linked listint_t list
+* free_listint2 - Frees a linked listint_t list
 * @head: Pointer to the head of the list
 */
 void free_listint2(listint_t **head)
 {
 	listint_t *ptr = *head;
 	listint_t *nextNode;
+
+	if (*head == NULL)
+	{
+		free(*head);
+	}
 
 	while (ptr != NULL)
 	{
