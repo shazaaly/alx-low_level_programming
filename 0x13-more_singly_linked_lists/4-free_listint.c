7c1a724669dee72c3f20a945e6540550e70abf29
root <root@shaza>
2023-05-02 17:51:31 +0200

    free

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,11 +2,15 @@
 #include "stdio.h"
 #include <stddef.h>
 #include <stdlib.h>
-
+/**
+* free_listint - Adds a new node at the end of a linked listint_t list
+* @head: Pointer to a pointer to the head of the list
+* Return: void
+*/
 void free_listint(listint_t *head)
 {
 	listint_t *ptr = head;
-    listint_t *nextNode;
+	listint_t *nextNode;
 
 	while (ptr != NULL)
 	{
