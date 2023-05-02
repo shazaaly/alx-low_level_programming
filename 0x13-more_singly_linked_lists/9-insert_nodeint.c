798581da1489e0a8b536235d37fbae1f79da56e6
root <root@shaza>
2023-05-03 01:29:19 +0200

    insert at index fix6

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		free(newNode);
 	}
 	newNode->n = n;
-	for (i = 0; ptr != NULL && i <= idx - 1; i++)
+	for (i = 0; i < idx - 1 && ptr != NULL; i++)
 	{
 		ptr = ptr->next;
 	}
