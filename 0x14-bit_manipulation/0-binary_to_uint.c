0e0228485e33db49ca73d5aeaf7610aabd517620
root <root@shaza>
2023-05-06 03:20:59 +0200

    left shift and while

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,18 +11,17 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0;
 
-	if (*b == '\0')
-	{
-		return (0);
-	}
-
-	if (*b != '0' && *b != '1')
+	if (b == NULL)
 	{
 		return (0);
 	}
 	while (*b != '\0')
 	{
-		n = n << 1 + (*b + '0');
+		if (*b != '0' && *b != '1')
+	{
+		return (0);
+	}
+		n = (n << 1) + (*b + '0');
 		b++;
 	}
 	return (n);
