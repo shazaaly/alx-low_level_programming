83d8ad526edb1de618879603d68c68779c8549f8
root <root@shaza>
2023-05-06 07:20:52 +0200

    get_endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* get_endianness - checks the endianness of the system
+* Return: 0 if big endian, 1 if little endian
+*/
+int get_endianness(void)
+{
+	int i = 1;
+	chat *ptr = (char *)&i;
+
+	if (ptr == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
+
+}
