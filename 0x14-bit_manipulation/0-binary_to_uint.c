99d9e17790843a0961a7ee5aa80e56c1aba10ccb
root <root@shaza>
2023-05-06 03:18:56 +0200

    left shift and while

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 #include <string.h>
-
+/**
+* binary_to_uint - converts a binary number to an unsigned int
+* @b: pointer to a string of 0 and 1 chars
+* Return: the converted number, or 0 if there is one or more chars in the
+* string b that is not 0 or 1, or if b is NULL.
+*/
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0;
@@ -15,18 +20,10 @@ unsigned int binary_to_uint(const char *b)
 	{
 		return (0);
 	}
-	n = binary_to_uint(b + 1);
-
-	if (*b == '1')
+	while (*b != '\0')
 	{
-		return (n << 1 | 1);
+		n = n << 1 + (*b + '0');
+		b++;
 	}
-	else if (*b == '0')
-	{
-		return (n << 1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (n);
 }
