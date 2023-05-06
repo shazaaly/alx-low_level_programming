476ce3b8de5c794fa07f9d818228143cca441953
root <root@shaza>
2023-05-06 06:03:26 +0200

    clear_bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,11 +8,12 @@
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask = 1UL << index;
+
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
-	unsigned long int mask = 1UL << index;
 	*n &= ~mask;
 	return (1);
 }
