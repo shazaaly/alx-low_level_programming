2187931b240243513d8745aac8b04b2be328a2d4
root <root@shaza>
2023-05-06 06:44:00 +0200

    flip_bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* flip_bits - Computes the number of bits that need to be flipped to transform
+* @n: The first unsigned long integer.
+* @m: The second unsigned long integer.
+* Return: The number of bits that need to be flipped to transform @n into @m.
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor;
+	unsigned long int count;
+
+	xor =  n ^ m;  /* puts 1 at different values posithions*/
+	while (xor != 0)
+	{
+		count += xor & 1; /*extract the least significant bit of the XOR*/
+		xor >>= 1; /*move loop*/
+	}
+	return (count);
+}
