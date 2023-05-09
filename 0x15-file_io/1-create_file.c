5ccd7b10c93fbf963bb5966a409c3486a0ae2504
root <root@shaza>
2023-05-09 04:45:47 +0200

    1-create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+* create_file - creates a file with the specified name
+* text content to it
+* @filename: the name of the file to create
+* @text_content: a null-terminated string
+* Return: 1 on success, -1 on failure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+	if (fd == -1)
+	{
+		return (-1);
+
+	}
+	bytes_written = write(fd, text_content, strlen(text_content));
+	if (bytes_written == -1)
+	{
+		close(fd);
+		return (-1);
+
+	}
+	close(fd);
+	return (-1);
+}
+
