2c770c97c5215b93c81e1d6f802abeb5da825e26
root <root@shaza>
2023-05-09 23:21:58 +0200

    mode

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,12 +16,13 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t bytes_written;
 	int len = 0;
+	mode_t mode = S_IRUSR | S_IWUSR;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
 	if (fd == -1)
 	{
 		return (-1);
