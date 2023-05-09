5df5903b145f0d6648f5bc2185332d68e7e7d9bc
root <root@shaza>
2023-05-09 04:49:20 +0200

    1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
 	if (fd == -1)
 	{
 		return (-1);
@@ -34,6 +34,6 @@ int create_file(const char *filename, char *text_content)
 
 	}
 	close(fd);
-	return (-1);
+	return (1);
 }
 
