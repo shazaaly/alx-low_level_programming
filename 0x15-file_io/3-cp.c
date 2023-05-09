0fd5f7bb3e84ce4abf8e3c7055218e7133bd30bd
root <root@shaza>
2023-05-10 00:41:52 +0200

    fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <errno.h>
+
 /**
 * main - entry point for the cp command program
 * @argc: the number of command-line arguments
@@ -64,9 +65,7 @@ int copy_file(const char *file_from_name, const char *file_to_name)
 		if (bytes_written != bytes_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to  %s\n", file_to_name);
-			close(fd);
 			fd_closer(fd);
-			close(fd1);
 			fd_closer(fd1);
 			exit(99);
 		}
@@ -75,16 +74,11 @@ int copy_file(const char *file_from_name, const char *file_to_name)
 	if (bytes_read == -1)
 	{
 	dprintf(STDERR_FILENO, "Error: Can't read from %s\n", file_from_name);
-	close(fd);
 	fd_closer(fd);
-	close(fd1);
 	fd_closer(fd1);
 	exit(98);
 	}
-
-	close(fd);
 	fd_closer(fd);
-	close(fd1);
 	fd_closer(fd1);
 	return (0);
 }
@@ -120,7 +114,6 @@ int open_dest(const char *file_to_name)
 	{
 		dprintf(STDERR_FILENO, "Can't write to %s\n",
 			file_to_name);
-			close(fd);
 			fd_closer(fd);
 			exit(99);
 	}
