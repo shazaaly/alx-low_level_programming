6fbc51cea2351a65c4d409fdda6ada638380f6d4
root <root@shaza>
2023-05-09 18:18:01 +0200

    cp file_from file_to

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -95,6 +95,7 @@ int open_source(const char *file_from_name)
 	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Can't read from file %s\n", file_from_name);
+		exit (98);
 	}
 	return (fd);
 }
@@ -113,6 +114,7 @@ int open_dest(const char *file_to_name)
 		dprintf(STDERR_FILENO, "Can't write to %s\n",
 			file_to_name);
 			fd_closer(fd);
+			exit (99);
 	}
 	return (fd);
 }
