2f981256ce1af9c08d25073a4374578944254a00
root <root@shaza>
2023-05-09 23:59:37 +0200

    fic strlrn

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,19 +16,23 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	int res = -1;
 	int len = 0;
 	ssize_t bytes_written;
 
 	if (filename == NULL)
 	{
 		return (-1);
-
 	}
 	fd = open(filename, O_WRONLY  | O_APPEND);
+
 	if (fd == -1)
 	{
 		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
 	}
 	if (text_content != NULL)
 	{
@@ -36,15 +40,18 @@ int append_text_to_file(const char *filename, char *text_content)
 		{
 			len++;
 		}
-		bytes_written = write(fd, text_content, strlen(text_content));
+		bytes_written = write(fd, text_content, len);
 		if (bytes_written == -1)
 		{
 			close(fd);
 			return (-1);
 		}
+		if (close(fd) == -1)
+		{
+		return (-1);
+		}
 	}
 	close(fd);
-	return (res);
-
+	return (1);
 }
 
