50ba84a163210e20ad6f9653ca7ae535182f56b8
root <root@shaza>
2023-05-10 02:11:54 +0200

    proto

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,9 +5,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int fd_closer(int fd);
-int copy_file(const char *file_from_name, const char *file_to_name);
-int open_source(const char *file_from_name);
-int open_dest(const char *file_to_name);
 void print_error(char* message);
 #endif
 
