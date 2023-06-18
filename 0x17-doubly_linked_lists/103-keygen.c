c06991758979737fc85fb3d7d78f58df1343e4e5
shazaaly <shaza.aly@gmail.com>
2023-06-18 21:00:58 +0000

    keygen kernel style edit

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,35 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-    char *input_str = NULL;
-    char *password = NULL;
-    int i, len;
+int main()
+{
 
-    if (argc < 2) {
-        printf("Usage: %s <input string>\n", argv[0]);
-        return (1);
+    char password[7]; char *codex = NULL;
+    int i;
+
+    codex = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    srand(time(NULL));
+
+    for (i = 0; i < 6; i++) {
+        password[i] = codex[rand() % 64];
     }
 
-    input_str = argv[1];
-    len = strlen(input_str);
-
-    password = malloc((len + 1) * sizeof(char));
-
-    for (i = 0; i < len; i++) {
-        password[i] = input_str[i] + 1;
-    }
-
-    for (; i < 8; i++) {
-        password[i] = 'A' + i;
-    }
-
-    password[9] = '\0';
-
+    password[6] = '\0';
     printf("%s\n", password);
-
-    free(password);
 
     return (0);
 }
