c235b06a883ec7fcf6904f19e6c0d7d5a4b10718
shazaaly <shaza.aly@gmail.com>
2023-06-19 21:30:56 +0300

    cp

---
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strpbrk - Searches a string for any of a set of bytes.
+*
+* @s: Pointer to the string to search.
+* @accept: Pointer to the string of accepted bytes.
+*
+* Return: A pointer to the byte in s that matches one of the bytes in accept
+*/
+char *_strpbrk(char *s, char *accept)
+{
+
+}
