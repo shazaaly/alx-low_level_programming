998834b65c6a646999f242f4437c7e9ad7b13803
shazaaly <shaza.aly@gmail.com>
2023-07-07 22:50:42 +0300

    malloc and NULL test

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,9 +10,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
-	table->array = calloc(size * sizeof(hash_node_t *));
+	table->array = malloc(size * sizeof(hash_node_t *));
 
-	if (!table)
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+
+	if (table->array == NULL)
 	{
 		return (NULL);
 	}
