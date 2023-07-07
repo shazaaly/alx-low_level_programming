7728213e16fdfe179da05582f94d6669bc9707ae
shazaaly <shaza.aly@gmail.com>
2023-07-07 23:49:38 +0300

    fix index init

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,8 +9,7 @@
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int = index;
-	unsigned long i;
+	unsigned long int index;
 
 	index = hash_djb2(key) % size;
 	return (index);
