0c9fff7002078bbc9d4d4ce8dbcb3536f5697028
shazaaly <shaza.aly@gmail.com>
2023-07-07 23:25:17 +0300

    1-djb2

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+* hash_djb2 - implements the DJB2 hash function
+* @str: string to hash
+*
+* Return: hash value of the string
+*/
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int index;
+
+	index = hash_djb2(str);
+
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -35,8 +35,12 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+
+
 /*prototypes*/
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int hash_djb2(const unsigned char *str);
 
 
 
