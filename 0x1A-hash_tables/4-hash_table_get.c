4798d8dcf2143538c95e278aff7c40502585daca
shazaaly <shaza.aly@gmail.com>
2023-07-08 02:29:35 +0300

    check for null

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,6 +11,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index = 0;
 	hash_node_t *tmp;
 
+	if (!ht || !key)
+	{
+		return (NULL);
+	}
+
 	index = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[index];
 
