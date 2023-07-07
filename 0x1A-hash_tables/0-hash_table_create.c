95ce07cd851c84f270ae2e524e4ee141e6d18b10
shazaaly <shaza.aly@gmail.com>
2023-07-07 23:02:06 +0300

    fix init

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,26 +8,25 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *table;
 	unsigned long int i;
-	hash_table_t *table = malloc(sizeof(hash_table_t));
-	table->array = malloc(size * sizeof(hash_node_t *));
+	table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
 	{
 		return (NULL);
 	}
-
+	table->size = size;
+	table->array = malloc(size * sizeof(hash_node_t *));
 	if (table->array == NULL)
 	{
 		free(table);
 		return (NULL);
 	}
 
-	table->size = size;
 	for (i = 0; i < size; i++)
 	{
 		table->array[i] = NULL;
-
 	}
 	return (table);
 }
