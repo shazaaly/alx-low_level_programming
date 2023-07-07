f5400ad549bc22769c9ad830de37a51f812f2d50
shazaaly <shaza.aly@gmail.com>
2023-07-08 01:09:24 +0300

    3-hash_table_set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_set - adds an element to the hash table
+* @ht: hash table to add element to
+* @key: key to add
+* @value: value to add
+*
+* Return: 1 on success, 0 on failure
+*/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = 0;
+	hash_node_t *node = NULL;
+
+	index = hash_djb2(key) % ht->size;
+
+	node = malloc(sizeof(hash_node_t))
+	if (node == NULL)
+	{
+		return (0);
+	}
+	node->key = key;
+	node->value = value;
+	node->next = NULL;
+
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = node;
+	}
+	else
+	{
+		node->next = ht->array[index];
+		ht->array[index] = node;
+
+	}
+	return (1);
+}
