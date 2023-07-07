d56965aeaf2559abcf34a7763064d04346609487
shazaaly <shaza.aly@gmail.com>
2023-07-07 22:48:02 +0300

    foo * fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+* hash_table_create - creates a new hash table with the specified size
+* @size: size of the hash table
+*
+* Return: a pointer to the newly created hash table.
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
