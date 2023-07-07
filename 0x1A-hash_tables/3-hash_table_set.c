4ccce40c72e75af465579d5b5079ad12bcd8a8af
shazaaly <shaza.aly@gmail.com>
2023-07-08 01:20:00 +0300

    cast

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,15 +14,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = 0;
 	hash_node_t *node = NULL;
 
-	index = hash_djb2(key) % ht->size;
+	index = hash_djb2((const unsigned char *)key) % (ht->size);
 
-	node = malloc(sizeof(hash_node_t))
+	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 	{
 		return (0);
 	}
-	node->key = key;
-	node->value = value;
+	node->key = (char *)key;
+	node->value = (char *)value;
 	node->next = NULL;
 
 	if (ht->array[index] == NULL)
