154d8bc80ee9f1e4469cc715701145437b32415a
shazaaly <shaza.aly@gmail.com>
2023-07-08 01:23:31 +0300

    check for null

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,6 +14,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = 0;
 	hash_node_t *node = NULL;
 
+	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
+	{
+		return (0);
+	}
+
 	index = hash_djb2((const unsigned char *)key) % (ht->size);
 
 	node = malloc(sizeof(hash_node_t));
