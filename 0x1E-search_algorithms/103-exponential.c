c6aab35bc7c8a684bff3febdce0985ce035c9b00
shazaaly <shaza.aly@gmail.com>
2023-10-11 22:19:29 +0300

    print

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -2,6 +2,27 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <math.h>
+
+/**
+* printArray - Prints the elements of an array.
+* @array: The array to be printed.
+* @size: The size of the array.
+*/
+void printArray(int *array, size_t size)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+	{
+		printf("%d", array[i]);
+		if (i < size - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
 /**
 * exponential_search - Search for a value in an array using exponential search.
 * Description: This function searches for the specified 'value'
@@ -11,24 +32,25 @@
 * Return: Index of the 'value' if found, or -1 if not found.
 */
 
+
 int exponential_search(int *array, size_t size, int value)
 {
 	size_t i = 1;
 	size_t mid;
-	size_t left, right;
+	size_t left, right = 0;
 
 	while (i < size && array[i] < value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		i *= 2;
-
 	}
-	left = 0;
+	left = right;
 	right = (i < size) ? i : size - 1;
 
 	while (left <= right)
 	{
 		mid = left + (right - left) / 2;
+
 		if (array[mid] == value)
 		{
 			return ((int)mid);
@@ -43,9 +65,10 @@ int exponential_search(int *array, size_t size, int value)
 		}
 			printf("Value found between indexes [%ld] and [%ld]\n", left, right);
 			return (1);
-
-
 	}
-
-	return (-1);
+		if (left <= right)
+		{
+			printArray(&array[left], right - left + 1);
+		}
+		return (-1);
 }
